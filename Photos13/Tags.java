b8f1e86d41e410b7ab468f6c80158148999df9b0
TheNicoleLe <94325842+TheNicoleLe@users.noreply.github.com>
2024-04-08 21:58:11 -0400

    Added base JavaDoc comments for Tags.java

--- Photos13/Tags.java
+++ Photos13/Tags.java
@@ -2,35 +2,69 @@ package Photos13;
 
 import java.io.Serializable;
 
+/**
+ * This class implements the tags functionality for photos
+ * @author Yue Luo
+ * @author Nicole Le
+ */
 public class Tags implements Serializable{
     private String TagName;
     private String TagValue;
     
+    /**
+     * Constructs a photo tag
+     * @param TagName
+     * @param TagValue
+     */
     public Tags(String TagName, String TagValue){
         this.TagName = TagName;
         this.TagValue = TagValue;
     }
     
+    /**
+     * Gets a tag name
+     * @return
+     */
     public String getTagName(){
         return TagName;
     }
     
+    /**
+     * Sets a tag name
+     * @param newName
+     */
     public void setTagName(String newName){
         TagName = newName;
     }
     
+    /**
+     * Gets a tag value
+     * @return
+     */
     public String getTagValue(){
         return TagValue;
     }
     
+    /**
+     * Sets a tag value
+     * @param newValue
+     */
     public void setTagValue(String newValue){
         TagValue = newValue;
     }
     
+    /**
+     * @return TagName and TagValue as String
+     */
     public String toString(){
         return TagName + " " + TagValue;
     }
     
+    /**
+     * Check if tags identical in name AND value
+     * @param tag
+     * @return
+     */
     public boolean equals(Tags tag){
         return this.TagName.equals(tag.getTagName()) && this.TagValue.equals(tag.getTagValue());
     }
