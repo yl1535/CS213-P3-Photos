6ee09ec5a7d3ab3fc91d2491f0011e6e8be773d0
TheNicoleLe <94325842+TheNicoleLe@users.noreply.github.com>
2024-04-08 21:51:44 -0400

    Added base Javadoc comments to Photos.java

--- Photos13/Photos.java
+++ Photos13/Photos.java
@@ -6,12 +6,23 @@ import javafx.scene.Scene;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
-public class Photos extends Application{
+/**
+ * The stage and scenes of photos
+ * @author Yue Luo
+ * @author Nicole Le
+ */
+
+public class Photos extends Application {
     
-    @Override
+    /**
+     * Start the stage
+     * @Override
+     * @param primaryStage
+     * @throws Exception
+     */
     public void start(Stage primaryStage) throws Exception {
 		
-        // create FXML loader
+    // create FXML loader
 	FXMLLoader loader = new FXMLLoader();
 	loader.setLocation(getClass().getResource("/Photos13/docs/Photos13UI.fxml"));   //requires path change when doing final correction on file path
 		
@@ -22,18 +33,22 @@ public class Photos extends Application{
 	Scene scene = new Scene(root);
 	primaryStage.setScene(scene);
 	primaryStage.setResizable(false);
-        primaryStage.setTitle("Photos Manager 13");
+    primaryStage.setTitle("Photos Manager 13");
 	primaryStage.show();
         
-        // set kill event
-        primaryStage.setOnCloseRequest(event -> Admin.SafelyQuit());
+     // set kill event
+    primaryStage.setOnCloseRequest(event -> Admin.SafelyQuit());
     }
-    
+
+   /**
+    * Main method to initialize and launch
+    * @param args
+    * @throws Exception
+    */ 
     public static void main(String[] args) throws Exception{
         Admin.initializeList();
         launch(args);
     }
     
     //ToDo: Update GUI Bug Report
-    
 }
